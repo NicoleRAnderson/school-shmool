24a4cb57ac8e654b6ef2138bdd8e6c368fe463eb
Nicole Anderson <nranderson98@gmail.com>
2021-11-11 12:37:38 -0700

    congress stuff ?

--- congress/congress.js
+++ congress/congress.js
@@ -3,41 +3,48 @@ import { senators } from "../data/senators.js";
 const senatorDiv = document.querySelector(".senators");
 
 function simplifiedSenators() {
-    return senators.map(senator => {
-        const middleName = senator.middle_name ? ` ${senator.middle_name} `: ` `
-        return {
-            id: senator.id,
-            name: `${senator.first_name}${middleName}${senator.last_name}`,
-            party: senator.party,
-            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
-            gender: senator.gender,
-            seniority: "later",
-            missedVotesPct: senator.missed_votes_pct,
-            loyaltyPct: senator.votes_with_party_pct,
-        }
-    })
+  return senators.map((senator) => {
+    const middleName = senator.middle_name ? ` ${senator.middle_name} ` : ` `;
+    return {
+      id: senator.id,
+      name: `${senator.first_name}${middleName}${senator.last_name}`,
+      party: senator.party,
+      imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-100px.jpeg`,
+      gender: senator.gender,
+      seniority: senator.seniority,
+      missedVotesPct: senator.missed_votes_pct,
+      loyaltyPct: senator.votes_with_party_pct,
+    };
+  });
 }
 
-populateSenatorDiv(simplifiedSenators(senators))
+populateSenatorDiv(simplifiedSenators(senators));
 
 function populateSenatorDiv(simpleSenators) {
-    simpleSenators.forEach(senator => {
-        let senFigure = document.createElement("figure")
-        let figImg = document.createElement("img")
-        let figCaption = document.createElement("figcaption")
+  simpleSenators.forEach((senator) => {
+    let senFigure = document.createElement("figure");
+    let figImg = document.createElement("img");
+    let figCaption = document.createElement("figcaption");
 
-        figImg.src = senator.imgURL
+    figImg.src = senator.imgURL;
 
-        figCaption.textContent = senator.name
-        senFigure.appendChild(figImg)
-        senFigure.appendChild(figCaption)
-        senatorDiv.appendChild(senFigure)
-    })
+    figCaption.textContent = senator.name;
+    senFigure.appendChild(figImg);
+    senFigure.appendChild(figCaption);
+    senatorDiv.appendChild(senFigure);
+  });
 }
 
-const filterSenators = (prop, value) => simplifiedSenators(senators).filter(senator => senator[prop] === value)
+const filterSenators = (prop, value) =>
+  simplifiedSenators(senators).filter((senator) => senator[prop] === value);
 
-const republicans = filterSenators("party", "R")
-const femaleSenators = filterSenators("gender", "F")
+const republicans = filterSenators("party", "R");
+const femaleSenators = filterSenators("gender", "F");
 
-console.log(republicans, femaleSenators)
+//console.log(republicans, femaleSenators)
+
+const mostSeniorSenator = simplifiedSenators().reduce((acc, senator) => {
+  return acc.seniority > senator.seniority ? acc : senator;
+});
+
+console.log(mostSeniorSenator)
